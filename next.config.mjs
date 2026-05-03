f0d5e07c3706b32b421e51549e2fdbd426e2799d
SoorajKrishna S <113533072+soorajzap@users.noreply.github.com>
2026-05-04 01:15:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ import { withContentCollections } from "@content-collections/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  output: "export", // Tells Next.js to build a static HTML site
+  // Add this if you ever use the <Image /> component from next/image
+  images: {
+    unoptimized: true,
+  },
+  // If your repo is named "my-portfolio" and it's NOT a username.github.io repo,
+  // you will also need to add a basePath:
+  // basePath: "/my-portfolio", 
 };
 
 // withContentCollections must be the outermost plugin
